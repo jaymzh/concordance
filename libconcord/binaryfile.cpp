956c853504bcf56e36e201c2654d44b1b25fed7e
Phil Dibowitz <phil@ipom.com>
2008-04-11 05:05:26 +0000

    Overview:   - Make copyright spacing consistent in all files   - Claim copyright in two files I wasn't in   - Spacing cleanup in win/usb_rtl.cpp
    Details:
    
    libconcord/win/usb_rtl.cpp
      - Significant style/spacing cleanup
    
    libconcord/usblan.cpp
    libconcord/web.cpp
      - claim copyright
    
    ALL FILES:
      - Fix copyright spacing
    
    
    - Phil

--- libconcord/binaryfile.cpp
+++ libconcord/binaryfile.cpp
@@ -1,27 +1,28 @@
 /*
- *  vi: formatoptions+=tc textwidth=80 tabstop=8 shiftwidth=8 noexpandtab:
+ * vi: formatoptions+=tc textwidth=80 tabstop=8 shiftwidth=8 noexpandtab:
  *
- *  $Id$
+ * $Id$
  *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 3 of the License, or
- *  (at your option) any later version.
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- *  You should have received a copy of the GNU General Public License along
- *  with this program; if not, write to the Free Software Foundation, Inc.,
- *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  *
- *  (C) Copyright Kevin Timmerman 2007
- *  (C) Copyright Phil Dibowitz 2007
+ * (C) Copyright Kevin Timmerman 2007
+ * (C) Copyright Phil Dibowitz 2007
  */
 
-// C++ iostreams will eat EOF chars, even when ios::binary is used with read() and write()
+// C++ iostreams will eat EOF chars, even when ios::binary is used with
+// read() and write()
 
 #include "libconcord.h"
 #include "lc_internal.h"
--- libconcord/win/usb_rtl.cpp
+++ libconcord/win/usb_rtl.cpp
@@ -1,23 +1,23 @@
 /*
- *  vi: formatoptions+=tc textwidth=80 tabstop=8 shiftwidth=8 noexpandtab:
+ * vi: formatoptions+=tc textwidth=80 tabstop=8 shiftwidth=8 noexpandtab:
  *
- *  $Id$
+ * $Id$
  *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 3 of the License, or
- *  (at your option) any later version.
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- *  You should have received a copy of the GNU General Public License along
- *  with this program; if not, write to the Free Software Foundation, Inc.,
- *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  *
- *  (C) Copyright Kevin Timmerman 2007
+ * (C) Copyright Kevin Timmerman 2007
  */
 
 #ifdef WINHID
@@ -57,60 +57,85 @@ int LinkUSB(void)
 #if _MSC_VER <= 1020
 	hKernel32=GetModuleHandle("kernel32.dll");
 
-	if(hKernel32) {
-		CancelIo=reinterpret_cast<TCancelIo*>(GetProcAddress(hKernel32,"CancelIo"));
+	if (hKernel32) {
+		CancelIo = 
+			reinterpret_cast<TCancelIo*>(
+				GetProcAddress(hKernel32, "CancelIo"));
 	} else {
 		return -5;
 	}
 
-	if(CancelIo==NULL)
+	if (CancelIo == NULL)
 		return -6;
 #endif
 
-	hSetupapi=LoadLibrary("setupapi.dll");
+	hSetupapi = LoadLibrary("setupapi.dll");
 
-	if(hSetupapi) {
-		rtlSetupDiDestroyDeviceInfoList=(TSetupDiDestroyDeviceInfoList*)GetProcAddress(hSetupapi,"SetupDiDestroyDeviceInfoList");
-		rtlSetupDiEnumDeviceInterfaces=(TSetupDiEnumDeviceInterfaces*)GetProcAddress(hSetupapi,"SetupDiEnumDeviceInterfaces");
-		rtlSetupDiGetClassDevs=(TSetupDiGetClassDevsA*)GetProcAddress(hSetupapi,"SetupDiGetClassDevsA");
-		rtlSetupDiGetDeviceInterfaceDetail=(TSetupDiGetDeviceInterfaceDetailA*)GetProcAddress(hSetupapi,"SetupDiGetDeviceInterfaceDetailA");
-	} else
+	if (hSetupapi) {
+		rtlSetupDiDestroyDeviceInfoList =
+			(TSetupDiDestroyDeviceInfoList*)
+			GetProcAddress(hSetupapi,
+				"SetupDiDestroyDeviceInfoList");
+		rtlSetupDiEnumDeviceInterfaces =
+			(TSetupDiEnumDeviceInterfaces*)
+			GetProcAddress(hSetupapi,
+				"SetupDiEnumDeviceInterfaces");
+		rtlSetupDiGetClassDevs = (TSetupDiGetClassDevsA*)
+			GetProcAddress(hSetupapi, "SetupDiGetClassDevsA");
+		rtlSetupDiGetDeviceInterfaceDetail =
+			(TSetupDiGetDeviceInterfaceDetailA*)
+			GetProcAddress(hSetupapi,
+				"SetupDiGetDeviceInterfaceDetailA");
+	} else {
 		return -1;
+	}
 
-	if(rtlSetupDiDestroyDeviceInfoList==NULL ||
-		rtlSetupDiEnumDeviceInterfaces==NULL ||
-		rtlSetupDiGetClassDevs==NULL ||
-		rtlSetupDiGetDeviceInterfaceDetail==NULL)
+	if(rtlSetupDiDestroyDeviceInfoList == NULL ||
+		rtlSetupDiEnumDeviceInterfaces == NULL ||
+		rtlSetupDiGetClassDevs == NULL ||
+		rtlSetupDiGetDeviceInterfaceDetail == NULL)
 		return -2;
 
-	hHid=LoadLibrary("hid.dll");
+	hHid = LoadLibrary("hid.dll");
 	
-	if(hHid) {
-		HidD_GetHidGuid=(THidD_GetHidGuid*)GetProcAddress(hHid,"HidD_GetHidGuid");
-		HidD_GetAttributes=(THidD_GetAttributes*)GetProcAddress(hHid,"HidD_GetAttributes");
-		HidD_GetManufacturerString=(THidD_GetManufacturerString*)GetProcAddress(hHid,"HidD_GetManufacturerString");
-		HidD_GetProductString=(THidD_GetProductString*)GetProcAddress(hHid,"HidD_GetProductString");
-		HidD_GetSerialNumberString=(THidD_GetSerialNumberString*)GetProcAddress(hHid,"HidD_GetSerialNumberString");
-		HidD_GetIndexedString=(THidD_GetIndexedString*)GetProcAddress(hHid,"HidD_GetIndexedString");
-		HidD_GetPreparsedData=(THidD_GetPreparsedData*)GetProcAddress(hHid,"HidD_GetPreparsedData");
-		HidD_FreePreparsedData=(THidD_FreePreparsedData*)GetProcAddress(hHid,"HidD_FreePreparsedData");
-		HidP_GetCaps=(THidP_GetCaps*)GetProcAddress(hHid,"HidP_GetCaps");
-		HidD_GetFeature=(THidD_GetFeature*)GetProcAddress(hHid,"HidD_GetFeature");
-		HidD_SetFeature=(THidD_SetFeature*)GetProcAddress(hHid,"HidD_SetFeature");
-	} else
+	if (hHid) {
+		HidD_GetHidGuid = (THidD_GetHidGuid*)GetProcAddress(hHid,
+			"HidD_GetHidGuid");
+		HidD_GetAttributes = (THidD_GetAttributes*)GetProcAddress(hHid,
+			"HidD_GetAttributes");
+		HidD_GetManufacturerString = (THidD_GetManufacturerString*)
+			GetProcAddress(hHid, "HidD_GetManufacturerString");
+		HidD_GetProductString = (THidD_GetProductString*)
+			GetProcAddress(hHid, "HidD_GetProductString");
+		HidD_GetSerialNumberString = (THidD_GetSerialNumberString*)
+			GetProcAddress(hHid, "HidD_GetSerialNumberString");
+		HidD_GetIndexedString = (THidD_GetIndexedString*)
+			GetProcAddress(hHid, "HidD_GetIndexedString");
+		HidD_GetPreparsedData = (THidD_GetPreparsedData*)
+			GetProcAddress(hHid, "HidD_GetPreparsedData");
+		HidD_FreePreparsedData = (THidD_FreePreparsedData*)
+			GetProcAddress(hHid, "HidD_FreePreparsedData");
+		HidP_GetCaps = (THidP_GetCaps*)
+			GetProcAddress(hHid, "HidP_GetCaps");
+		HidD_GetFeature = (THidD_GetFeature*)
+			GetProcAddress(hHid, "HidD_GetFeature");
+		HidD_SetFeature = (THidD_SetFeature*)
+			GetProcAddress(hHid, "HidD_SetFeature");
+	} else {
 		return -3;
+	}
 	
-	if(HidD_GetHidGuid==NULL ||
-		HidD_GetAttributes==NULL ||
-		HidD_GetManufacturerString==NULL ||
-		HidD_GetProductString==NULL ||
-		HidD_GetSerialNumberString==NULL ||
-		HidD_GetIndexedString==NULL ||
-		HidD_GetPreparsedData==NULL ||
-		HidD_FreePreparsedData==NULL ||
-		HidP_GetCaps==NULL ||
-		HidD_GetFeature==NULL ||
-		HidD_SetFeature==NULL)
+	if (HidD_GetHidGuid == NULL ||
+		HidD_GetAttributes == NULL ||
+		HidD_GetManufacturerString == NULL ||
+		HidD_GetProductString == NULL ||
+		HidD_GetSerialNumberString == NULL ||
+		HidD_GetIndexedString == NULL ||
+		HidD_GetPreparsedData == NULL ||
+		HidD_FreePreparsedData == NULL ||
+		HidP_GetCaps == NULL ||
+		HidD_GetFeature == NULL ||
+		HidD_SetFeature == NULL)
 		return -4;
 
 	return 0;
@@ -118,10 +143,13 @@ int LinkUSB(void)
 
 void UnlinkUSB(void)
 {
-	if(hSetupapi) FreeLibrary(hSetupapi);
-	if(hHid) FreeLibrary(hHid);
-	if(hKernel32) FreeLibrary(hKernel32);
-	hSetupapi=hHid=hKernel32=NULL;
+	if (hSetupapi)
+		FreeLibrary(hSetupapi);
+	if (hHid)
+		FreeLibrary(hHid);
+	if (hKernel32)
+		FreeLibrary(hKernel32);
+	hSetupapi = hHid = hKernel32 = NULL;
 }
 
 #endif
