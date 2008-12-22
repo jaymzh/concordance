e88732d3d7f7581f428c42200b2ad0b94095349b
Phil Dibowitz <phil@ipom.com>
2008-12-22 23:15:07 +0000

    This patch cleanups up and adds stuff to consnoop to help in update-firmware debugging.
    consnoop/consnoop.cpp:
      - Bring the code into ... mostly CodingStyle compliance
      - Add commandline argument parsing
      - Add verbose and debug modes
      - General cleanups
    
    Signed-off-by: Phil Dibowitz <phil@ipom.com>

--- consnoop/consnoop.cpp
+++ consnoop/consnoop.cpp
@@ -23,25 +23,35 @@
 
 #include <string>
 #include <fstream>
+#include <stdlib.h>
+#ifdef WIN32
+typedef unsigned char uint8_t;
+#include "../concordance/win/getopt/getopt.h"
+#else
+#include <getopt.h>
+#endif
+
 using namespace std;
 
 #include "../libconcord/protocol.h"
 
-#ifdef WIN32
-typedef unsigned char uint8_t;
-#endif
+bool verbose = false;
+bool debug = false;
 
-int HexToInt(char c)
+int hex_to_int(char c)
 {
-	if(c>='0' && c<='9') return c-'0';
-	if(c>='a' && c<='f') return c-'a'+10;
-	if(c>='A' && c<='F') return c-'A'+10;
+	if (c >= '0' && c <= '9')
+		return c-'0';
+	if (c >= 'a' && c <= 'f')
+		return c-'a'+10;
+	if (c >= 'A' && c <= 'F')
+		return c-'A'+10;
 	return -1;
 }
 
-const char* GetMisc(uint8_t x)
+const char* get_misc(uint8_t x)
 {
-	switch(x) {
+	switch (x) {
 		case COMMAND_MISC_EEPROM:
 			return "EEPROM";
 		case COMMAND_MISC_STATE:
@@ -68,29 +78,47 @@ const char* GetMisc(uint8_t x)
 	return "Unknown";
 }
 
-void Decode(const uint8_t * const data)
+void print_data(const uint8_t * const data)
+{
+	if (!verbose)
+		return;
+	const uint8_t length = data[0] & LENGTH_MASK;
+	printf("   DATA: ");
+	for (int i = 0; i < length; i++) {
+		printf("%i", data[i]);
+	}
+	printf("\n");
+}
+
+void decode(const uint8_t * const data)
 {
 	// Note: The uninteresting stuff is commented out
-	const uint8_t length=data[0] & LENGTH_MASK;
-	switch(data[0] & COMMAND_MASK) {
+	const uint8_t length = data[0] & LENGTH_MASK;
+	switch (data[0] & COMMAND_MASK) {
 		case COMMAND_GET_VERSION & COMMAND_MASK:
 			printf("Get   Version\n");
 			break;
 		case RESPONSE_VERSION_DATA & COMMAND_MASK:
 			printf("Get   Version Response %02X %02X %02X %02X %02X %02X %02X\n",
-				data[1],data[2],data[3],data[4],data[5],data[6],data[7]);
+				data[1],data[2],data[3],data[4],data[5],
+				data[6],data[7]);
 			break;
 		case COMMAND_WRITE_FLASH & COMMAND_MASK:
-			printf("Write Flash %06X %i\n",data[1]<<16|data[2]<<8|data[3],data[4]<<8|data[5]);
+			printf("Write Flash %06X %i\n",
+				data[1]<<16|data[2]<<8|data[3],
+				data[4]<<8|data[5]);
 			break;
 		case COMMAND_WRITE_FLASH_DATA & COMMAND_MASK:
-			//printf("Write Flash Data\n");
+			printf("Write Flash Data\n");
+			print_data(data);
 			break;
 		case COMMAND_READ_FLASH & COMMAND_MASK:
-			printf("Read  Flash %06X %i\n",data[1]<<16|data[2]<<8|data[3],data[4]<<8|data[5]);
+			printf("Read  Flash %06X %i\n",
+				data[1]<<16|data[2]<<8|data[3],
+				data[4]<<8|data[5]);
 			break;
 		case RESPONSE_READ_FLASH_DATA & COMMAND_MASK:
-			//printf("Read  Flash Data\n");
+			printf("Read  Flash Data\n");
 			break;
 		case COMMAND_START_IRCAP & COMMAND_MASK:
 			printf("Start IR capture\n");
@@ -99,38 +127,42 @@ void Decode(const uint8_t * const data)
 			printf("Stop  IR capture\n");
 			break;
 		case RESPONSE_IRCAP_DATA & COMMAND_MASK:
-			//printf("IR Capture Data\n");
+			printf("IR Capture Data\n");
 			break;
 		case COMMAND_WRITE_MISC & COMMAND_MASK:
 			if(length==1)
-				printf("Write %s\n",GetMisc(data[1]));
+				printf("Write %s\n",get_misc(data[1]));
 			else if(length==3)
 				printf("Write %s %02X %02X\n",
-					GetMisc(data[1]),data[2],data[3]);
+					get_misc(data[1]), data[2], data[3]);
 			else
 				printf("Write %s %02X %02X %02X %02X\n",
-					GetMisc(data[1]),data[2],data[3],data[4],data[5]);
+					get_misc(data[1]), data[2], data[3],
+					data[4], data[5]);
 			break;
 		case COMMAND_READ_MISC & COMMAND_MASK:
 			if(length==2)
-				printf("Read  %s %02X\n",GetMisc(data[1]),data[2]);
+				printf("Read  %s %02X\n",
+					get_misc(data[1]), data[2]);
 			else
-				printf("Read  %s %02X %02X\n",GetMisc(data[1]),data[2],data[3]);
+				printf("Read  %s %02X %02X\n",
+					get_misc(data[1]), data[2], data[3]);
 			break;
 		case RESPONSE_READ_MISC_DATA & COMMAND_MASK:
-			/*
 			if(length==2)
-				printf("Read  %s Data %02X\n",GetMisc(data[1]),data[2]);
+				printf("Read  %s Data %02X\n",
+					get_misc(data[1]), data[2]);
 			else
-				printf("Read  %s Data %02X %02X\n",GetMisc(data[1]),data[2],data[3]);
-			*/
+				printf("Read  %s Data %02X %02X\n",
+					get_misc(data[1]), data[2], data[3]);
 			break;
 		case COMMAND_ERASE_FLASH & COMMAND_MASK:
-			printf("Erase Flash %06X\n",data[1]<<16|data[2]<<8|data[3]);
+			printf("Erase Flash %06X\n",
+				data[1]<<16|data[2]<<8|data[3]);
 			break;
 		case COMMAND_RESET & COMMAND_MASK:
 			printf("Reset ");
-			switch(data[1]) {
+			switch (data[1]) {
 				case COMMAND_RESET_USB:
 					printf("USB (01)\n");
 					break;
@@ -144,24 +176,52 @@ void Decode(const uint8_t * const data)
 					printf("Test Finish (04)\n");
 					break;
 				default:
-					printf("Unknown (%02X)\n",data[1]);
+					printf("Unknown (%02X)\n", data[1]);
 					break;
 			}
 			break;
 		case COMMAND_DONE & COMMAND_MASK:
-			//printf("Done %02X\n",data[1]);
+			printf("Done %02X\n", data[1]);
 			break;
 	}
 }
 
+void help()
+{
+	printf("Usage: consnoop <options>\n\n");
+
+	printf("Options:\n");
+	printf("\t-v\tVerbose. Print bytes we write.\n");
+	printf("\t-d\tDebug. Print full data for all decoded packets.\n");
+	printf("\t-f <file>\tFilename to parse.\n");
+	printf("\t-h\tThis help.\n\n");
+}
+
 int main(int argc, char *argv[])
 {
-	char *file_name=NULL;
-	for(int a=1; a<argc; ++a) {
-		if(argv[a][0]!='-' && file_name==NULL) file_name=argv[a];
+	int tmpint = 0;
+	char *file_name = NULL;
+	while ((tmpint = getopt(argc, argv, "dhf:v")) != EOF) {
+		switch (tmpint) {
+		case 'd':
+			debug = true;
+			break;
+		case 'f':
+			if (optarg == NULL) {
+				fprintf(stderr, "Missing file name.\n");
+			}
+			file_name = optarg;
+			break;
+		case 'h':
+			help();
+			exit(0);
+			break;
+		case 'v':
+			verbose = true;
+			break;
+		}
 	}
-	if (file_name==NULL)
-		file_name = (char *)"Log.xml";
+	printf("filename is %s", file_name);
 
 	ifstream infile;
 	infile.open(file_name);
@@ -171,28 +231,41 @@ int main(int argc, char *argv[])
 
 	while(!infile.eof()) {
 		getline(infile,s);
-		if(!s.compare(0,payloadbytes.size(),payloadbytes)) {
+		if(!s.compare(0, payloadbytes.size(), payloadbytes)) {
 			uint8_t data[260];
-			unsigned int n=0;
-			for(unsigned int i=payloadbytes.size(); i<s.size(); ++i) {
+			unsigned int n = 0;
+			for(unsigned int i = payloadbytes.size();
+                            i<s.size(); ++i) {
 				const char c=s[i];
-				if(c=='<') break;
-				const int h=HexToInt(c);
-				if(h<0) continue;
-				if(n&1)
+				if (c == '<')
+					break;
+				const int h = hex_to_int(c);
+				if (h < 0)
+					continue;
+				if (n & 1)
 					data[n>>1]|=h;
 				else
 					data[n>>1]=h<<4;
 				++n;
-				if(n>=sizeof(data)*2) break;
+				if (n >= sizeof(data)*2)
+					break;
 			}
-			n>>=1;
-			if(!n) continue;
-			//for(i=0; i<n; ++i) printf("%02X",data[i]); printf("\n"); // Show all payload bytes for debugging
-			if(data[0]==0x12) continue; // hack - Ignore USB descriptor
-			Decode(data);
+			n >>= 1;
+			if (!n)
+				continue;
+			// hack - Ignore USB descriptor
+			if (data[0] == 0x12)
+				continue;
+			if (debug) {
+				for (unsigned int i = 0; i < n; ++i) {
+					printf("%02X",data[i]);
+				}
+				printf("\n");
+			}
+			decode(data);
 		}
 	}
 	infile.close();
 	return 0;
 }
+
