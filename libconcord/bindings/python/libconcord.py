6489a1bf58cc30a3ac810b7b2336db3c3b33e752
Phil Dibowitz <phil@ipom.com>
2010-08-14 16:34:22 +0000

    Change the python ABI requireent to 1.1.0.
    Signed-off-by: Phil Dibowitz <phil@ipom.com>

--- libconcord/bindings/python/libconcord.py
+++ libconcord/bindings/python/libconcord.py
@@ -32,7 +32,7 @@ debug = (os.environ.get("LIBCONCORD_PY_TRACE", "0") == "1")
 # Define the libconcord ABI this libconcord.py corresponds to
 # Bump this when the .so file version gets bumped
 
-ABI_VERSION = 1
+ABI_VERSION = "1.1.0"
 
 # Load the DLL
 
