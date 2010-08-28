ec07682291938cc8baaca5fe3cc3c0d49b3670cf
Phil Dibowitz <phil@ipom.com>
2010-08-28 16:46:05 +0000

    concordance/concordance.c   - Add '+CVS' to version number for clarity of anyone using CVS.
    Signed-off-by: Phil Dibowitz <phil@ipom.com>

--- concordance/concordance.c
+++ concordance/concordance.c
@@ -112,7 +112,7 @@ int set_canon(int flag)
 #define DEFAULT_FW_FILENAME_BIN "firmware.bin"
 #define DEFAULT_SAFE_FILENAME "safe.bin"
 
-const char * const VERSION = "0.23";
+const char * const VERSION = "0.23+CVS";
 
 struct options_t {
 	int binary;
