7c40c9ee878c70bdce9b69d0ec6c72210b4b52cb
Phil Dibowitz <phil@ipom.com>
2012-03-19 05:41:00 +0000

    Version bump.
    Signed-off-by: Phil Dibowitz <phil@ipom.com>

--- concordance/concordance.c
+++ concordance/concordance.c
@@ -112,7 +112,7 @@ int set_canon(int flag)
 #define DEFAULT_FW_FILENAME_BIN "firmware.bin"
 #define DEFAULT_SAFE_FILENAME "safe.bin"
 
-const char * const VERSION = "0.23+CVS";
+const char * const VERSION = "0.24";
 
 struct options_t {
 	int binary;
