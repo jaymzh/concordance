b968b48fff1e5da7d7ce0fbdfd472f068a055fb0
Phil Dibowitz <phil@ipom.com>
2012-03-29 03:15:05 +0000

    From: Scott Talbert <swt@techie.net>
    Fixes several problems relating to the udev rules and dhcpd script for usbnet
    remotes.  Changes include:
    * The udev rule for usbnet remotes depends on the 75-net-description.rules.
      Moved the usbnet rule to a separate file so that it can occur later in rules
      processing and the other udev rules can stay where they are.
    * The start_concordance* files needed to be installed as executable.
    * On Fedora, there wasn't a path set when the start_concordance_dhcpd.sh script
      was run, so it couldn't find any of the programs.  Added a path.
    * Added an iptables rule to allow the DHCPD to receive requests (it was blocked
      by default on Fedora).
    * Added debug statements to start_concordance_dhcpd.sh script.
    * The start_concordance_dhcpd.sh script could write to stderr, which caused it
      to hang up udev.  Redirected output to /dev/null by default.
    
    Signed-off-by: Scott Talbert <swt@techie.net>
    Signed-off-by: Phil Dibowitz <phil@ipom.com>

--- libconcord/start_concordance_dhcpd.sh
+++ libconcord/start_concordance_dhcpd.sh
@@ -6,6 +6,8 @@ NM_WAIT_COUNT=3
 LOCAL_IP="169.254.1.1"
 REMOTE_IP="169.254.1.2"
 NETMASK="255.255.0.0"
+export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+IPTABLES_RULE="INPUT -p udp -i $INTERFACE --dport 67 -j ACCEPT"
 NMCLI=$(which nmcli)
 
 check_nm() {
@@ -22,11 +24,14 @@ check_nm() {
 if [ "$ACTION" = "add" ]; then
     check_nm
     if [ "$?" -eq 1 ]; then
+        echo "NetworkManager found running." >>$LOG
         COUNT=0
         while [ "$COUNT" -lt "$NM_WAIT_COUNT" ]; do
+            echo "Waiting for NetworkManager." >>$LOG
             $NMCLI -t -f DEVICE,STATE dev \
                 | grep "^${INTERFACE}\:connecting" >>$LOG 2>&1
             if [ "$?" = "0" ]; then
+                echo "Disconnecting $INTERFACE via NetworkManager." >>$LOG
                 $NMCLI dev disconnect iface $INTERFACE >>$LOG 2>&1
                 break
             fi
@@ -34,9 +39,14 @@ if [ "$ACTION" = "add" ]; then
             sleep 1
         done
     fi
+    echo "Configuring $INTERFACE interface." >>$LOG
     ifconfig $INTERFACE $LOCAL_IP netmask $NETMASK >>$LOG 2>&1
+    echo "Adding iptables rule." >>$LOG
+    iptables -I $IPTABLES_RULE >>$LOG 2>&1
     dnsmasq --port=0 --interface=$INTERFACE --bind-interfaces --leasefile-ro \
             --dhcp-range=$REMOTE_IP,$REMOTE_IP --pid-file=$PID_FILE
 else
+    echo "Stopping dnsmasq and removing iptables rule." >>$LOG
     kill $(cat $PID_FILE)
+    iptables -D $IPTABLES_RULE >>$LOG 2>&1
 fi
