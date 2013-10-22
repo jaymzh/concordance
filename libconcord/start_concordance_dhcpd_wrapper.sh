2ac3c75d870fbd910b3411fe74cbdf193a25bfd2
Scott Talbert <swt@techie.net>
2013-10-21 23:07:04 -0400

    Fix startup of USBNet DHCP server by udev
    A change was made to udev where it now kills off any processes that are in the
    same cgroup that have been running for more than a few seconds (thus our DHCP
    server was getting killed off).  Instead ask 'at' to launch our DHCP server so
    that udev does not kill it off.
    
    Signed-off-by: Scott Talbert <swt@techie.net>
    Signed-off-by: Phil Dibowitz <phil@ipom.com>

--- libconcord/start_concordance_dhcpd_wrapper.sh
+++ libconcord/start_concordance_dhcpd_wrapper.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Kicks off dhcpd startup in the background so udev can go about its work.
-$(dirname $0)/start_concordance_dhcpd.sh >/dev/null 2>&1 &
+echo $(dirname $0)/start_concordance_dhcpd.sh | at now
