4b72932039bde74c66b85d79884623da7af6636b
Scott Talbert <swt@techie.net>
2014-01-02 20:39:39 -0500

    Needed to add c++-mode to .dir-locals also.
    Signed-off-by: Scott Talbert <swt@techie.net>
    Signed-off-by: Phil Dibowitz <phil@ipom.com>

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,2 +1,4 @@
 ((c-mode . ((c-basic-offset . 4)
-            (indent-tabs-mode . nil))))
+            (indent-tabs-mode . nil)))
+ (c++-mode . ((c-basic-offset . 4)
+              (indent-tabs-mode . nil))))
